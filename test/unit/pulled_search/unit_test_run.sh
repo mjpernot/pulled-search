0a4db2ec1df4fcd47aad3e04129105759ee698a7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-04 09:45:36 +0000

    Add new file unit_test_run.sh v0.0.1

---
+++ test/unit/pulled_search/unit_test_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Unit testing program for the pulled_search.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit test:  pulled_search.py"
+test/unit/check_log/help_message.py
