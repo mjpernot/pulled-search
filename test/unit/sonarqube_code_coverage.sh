bd45fa321444a010d778401bb4f736ae7e55ba92
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-06 12:17:44 +0000

    Update sonarqube_code_coverage.sh v0.0.1

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -12,6 +12,7 @@ coverage run -a --source=pulled_search test/unit/pulled_search/create_json.py
 coverage run -a --source=pulled_search test/unit/pulled_search/create_rmq.py
 coverage run -a --source=pulled_search test/unit/pulled_search/help_message.py
 coverage run -a --source=pulled_search test/unit/pulled_search/non_processed.py
+coverage run -a --source=pulled_search test/unit/pulled_search/main.py
 coverage run -a --source=pulled_search test/unit/pulled_search/process_docid.py
 coverage run -a --source=pulled_search test/unit/pulled_search/process_files.py
 coverage run -a --source=pulled_search test/unit/pulled_search/run_program.py
