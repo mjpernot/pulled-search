6e591daccab1c4084d46c221491dd144770ab99e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-09 17:36:19 +0000

    Update test/unit/pulled_search/unit_test_run.sh

--- test/unit/pulled_search/unit_test_run.sh
+++ test/unit/pulled_search/unit_test_run.sh
@@ -8,7 +8,7 @@ echo ""
 echo "Unit test:  pulled_search.py"
 test/unit/pulled_search/create_json.py
 test/unit/pulled_search/create_rmq.py
-test/unit/check_log/help_message.py
+test/unit/pulled_search/help_message.py
 test/unit/pulled_search/main.py
 test/unit/pulled_search/non_processed.py
 test/unit/pulled_search/process_docid.py
