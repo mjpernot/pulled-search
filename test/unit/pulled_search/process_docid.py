ab508bc553caea349c2792fac0c15433659916de
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-10 14:58:31 +0000

    Update test/unit/pulled_search/process_docid.py

--- test/unit/pulled_search/process_docid.py
+++ test/unit/pulled_search/process_docid.py
@@ -85,17 +85,18 @@ class UnitTest(unittest.TestCase):
                 self.outfile = "/dir/path/outfile"
 
         self.cfg = CfgTest()
+        self.args_array = {}
         self.data_list = ['{',
                           '"docid": "weotiuer",',
                           '"command": "COMMAND",',
-                          '"postdate": "20200102-101134"',
+                          '"pubdate": "20200102-101134"',
                           '}']
         self.file_log = ["Line1", "Line2", "Line3"]
         self.fname = "/dir_path/092438k234_docid.json"
         self.docid_dict = {"docid": "weotiuer", "command": "COMMAND",
-                           "postdate": "20200102-101134"}
+                           "pubdate": "20200102-101134"}
         self.log_json = {"docID": "weotiuer", "command": "COMMAND",
-                         "postDate": "20200102-101134",
+                         "pubDate": "20200102-101134",
                          "securityEnclave": "ENCLAVE",
                          "asOf": "20200306 084503", "serverName": "SERVERNAME",
                          "logEntries": ["line1", "line2", "line3"]}
@@ -127,7 +128,7 @@ class UnitTest(unittest.TestCase):
         mock_match.return_value = self.log_files
 
         self.assertEqual(pulled_search.process_docid(
-            self.cfg, self.fname, mock_log), False)
+            self.args_array, self.cfg, self.fname, mock_log), False)
 
     @mock.patch("pulled_search.check_log.run_program",
                 mock.Mock(return_value=True))
@@ -153,7 +154,7 @@ class UnitTest(unittest.TestCase):
         mock_match.return_value = self.log_files
 
         self.assertEqual(pulled_search.process_docid(
-            self.cfg, self.fname, mock_log), True)
+            self.args_array, self.cfg, self.fname, mock_log), True)
 
     @mock.patch("pulled_search.check_log.run_program",
                 mock.Mock(return_value=True))
@@ -181,7 +182,7 @@ class UnitTest(unittest.TestCase):
         mock_match.return_value = self.log_files
 
         self.assertEqual(pulled_search.process_docid(
-            self.cfg, self.fname, mock_log), True)
+            self.args_array, self.cfg, self.fname, mock_log), True)
 
 
 if __name__ == "__main__":
