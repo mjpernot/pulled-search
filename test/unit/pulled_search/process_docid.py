629a4d9942127400ba6a3da2c60a4cb9217b8df2
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-11 10:48:43 +0000

    Update process_docid.py v0.0.2

--- test/unit/pulled_search/process_docid.py
+++ test/unit/pulled_search/process_docid.py
@@ -43,6 +43,7 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_rm_file_failed -> Test with failure to remove outfile.
         test_rabbitmq_failed -> Test with failure to send to RabbitMQ.
         test_file_empty -> Test with no log entries returned.
         test_with_data -> Test with successful log file check.
@@ -101,6 +102,34 @@ class UnitTest(unittest.TestCase):
                          "asOf": "20200306 084503", "serverName": "SERVERNAME",
                          "logEntries": ["line1", "line2", "line3"]}
         self.log_files = ["/path/logfile1", "/path/logfile2"]
+
+    @mock.patch("pulled_search.check_log.run_program",
+                mock.Mock(return_value=True))
+    @mock.patch("pulled_search.send_2_rabbitmq", mock.Mock(return_value=True))
+    @mock.patch("pulled_search.create_json", mock.Mock(return_value=True))
+    @mock.patch("pulled_search.gen_libs.rm_file",
+                mock.Mock(return_value=(False, "Error Message")))
+    @mock.patch("pulled_search.gen_libs.is_empty_file",
+                mock.Mock(return_value=False))
+    @mock.patch("pulled_search.gen_libs.dir_file_match")
+    @mock.patch("pulled_search.gen_libs.file_2_list")
+    @mock.patch("pulled_search.gen_class.Logger")
+    def test_rm_file_failed(self, mock_log, mock_list, mock_match):
+
+        """Function:  test_rm_file_failed
+
+        Description:  Test with failure to remove outfile.
+
+        Arguments:
+
+        """
+
+        mock_log.return_value = True
+        mock_list.side_effect = [self.data_list, self.file_log]
+        mock_match.return_value = self.log_files
+
+        self.assertEqual(pulled_search.process_docid(
+            self.args_array, self.cfg, self.fname, mock_log), True)
 
     @mock.patch("pulled_search.check_log.run_program",
                 mock.Mock(return_value=True))
