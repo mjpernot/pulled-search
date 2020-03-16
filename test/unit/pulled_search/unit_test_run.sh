b91f56c3665b3cb84cb41830386af6233ddc9f83
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-16 16:38:28 +0000

    Update test/unit/pulled_search/unit_test_run.sh v0.1.0

--- test/unit/pulled_search/unit_test_run.sh
+++ test/unit/pulled_search/unit_test_run.sh
@@ -17,6 +17,7 @@ test/unit/pulled_search/month_days.py
 test/unit/pulled_search/non_processed.py
 test/unit/pulled_search/process_docid.py
 test/unit/pulled_search/process_files.py
+test/unit/pulled_search/process_list.py
 test/unit/pulled_search/run_program.py
 test/unit/pulled_search/send_2_rabbitmq.py
 test/unit/pulled_search/setup_mail.py
