11d4ab85a8e1614b8586da15c75601bfb20de5cf
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-03-16 15:57:39 +0000

    Update pulled_search.process_docids v0.1.0

--- pulled_search.py
+++ pulled_search.py
@@ -425,7 +425,7 @@ def setup_mail(args_array, subj=None, **kwargs):
     return mail
 
 
-def process_docids(args_array, cfg. log, docid_files, **kwargs):
+def process_docids(args_array, cfg, log, docid_files, **kwargs):
 
     """Function:  process_docids
 
