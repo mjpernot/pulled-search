522d5cb2b9e21eca0e3694aad3991f1126258651
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-02 14:37:42 +0100

    Update test/unit/pulled_search/unit_test_run.sh v0.1.2

--- test/unit/pulled_search/unit_test_run.sh
+++ test/unit/pulled_search/unit_test_run.sh
@@ -17,7 +17,6 @@ test/unit/pulled_search/help_message.py
 test/unit/pulled_search/insert_data.py
 test/unit/pulled_search/main.py
 test/unit/pulled_search/mvalidate_dirs.py
-test/unit/pulled_search/month_days.py
 test/unit/pulled_search/non_processed.py
 test/unit/pulled_search/process_docid.py
 test/unit/pulled_search/process_files.py
