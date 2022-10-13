ce1b5e7f9708f4d5a30e9b870bd10739adee4231
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2022-10-13 16:06:34 +0100

    Update pulled_search.py v0.1.4

--- pulled_search.py
+++ pulled_search.py
@@ -240,12 +240,21 @@ import platform
 import decimal
 
 # Local
-import lib.gen_libs as gen_libs
-import lib.gen_class as gen_class
-import rabbit_lib.rabbitmq_class as rabbitmq_class
-import mongo_lib.mongo_libs as mongo_libs
-import checklog.check_log as check_log
-import version
+try:
+    from .lib import gen_libs
+    from .lib import gen_class
+    from .rabbit_lib import rabbitmq_class
+    from .mongo_lib import mongo_libs
+    from .checklog import check_log
+    from . import version
+
+except (ValueError, ImportError) as err:
+    import lib.gen_libs as gen_libs
+    import lib.gen_class as gen_class
+    import rabbit_lib.rabbitmq_class as rabbitmq_class
+    import mongo_lib.mongo_libs as mongo_libs
+    import checklog.check_log as check_log
+    import version
 
 __version__ = version.__version__
 
